139c9be2252b6e023b9a3a4bfc5c3ae3353e5f01
l3l0ch <118216413+l3l0ch@users.noreply.github.com>
2023-06-09 20:39:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+diretorio = Diretorio('Projeto')
+chave = 'abc'
+escolha_uma_senha = input('Senha: ')
+senha_criptografada = cryptocode.encrypt(escolha_uma_senha, chave)
+a = Arquivo('teste','txt','senhas', senha_criptografada, diretorio)
+
+lista = ListaEncadeada()
+lista.inserir_no_inicio(a)
+lista.imprime_lista()
+
+
+def criar_arquivo(x):
+    with open ( f"a.txt", 'a' ) as arquivo:
+        arquivo.write ( f'senha:{x}' )
+
+criar_arquivo(a)
 
