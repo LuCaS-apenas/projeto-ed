73c5d6fca7bb5702c8b9b4cb2dd4bf18c6a8fc8d
l3l0ch <118216413+l3l0ch@users.noreply.github.com>
2023-06-09 20:29:35 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,22 +1,23 @@
 class Arquivo:
 
-    def __init__(self, nome: str, tipo: str, descricao: str, conteudo: str, criacao: datetime, diretorio:Diretorio, tamanho: int = 0):
+    def __init__(self, nome: str, tipo: str, descricao: str, conteudo: str, diretorio: Diretorio):
         self.nome = nome
         self.tipo = tipo
         self.descricao = descricao
-        self.tamanho = tamanho
+        self.tamanho = f'{sys.getsizeof ( conteudo )} Bytes'
         self.conteudo = conteudo
-        self.criacao = criacao
-        self.diretorio = diretorio
+        self.criacao = datetime.datetime.now()
+        self.diretorio = diretorio.nome
 
     def criar_arquivo(self):
-        with open ( self.nome, 'w' ) as arquivo:
-            arquivo.write (f'{self.conteudo}')
+        with open ( f'{self.nome}.txt', 'a' ) as arquivo:
+            arquivo.write (f'senha:{self.conteudo}')
 
     def __str__(self):
         return f'''
+        Diretório: {self.diretorio}
         Arquivo: {self.nome}.{self.tipo}
         Criado em: {self.criacao}
         Tamanho: {self.tamanho}
-        {self.conteudo}
+        Senha: {self.conteudo}
         '''
