13732efebecd42f07cc5d389371c6d11b3015805
ramonlbb <128474790+ramonlbb@users.noreply.github.com>
2023-06-14 20:03:18 -0300

    Update chaveCriptografia.py

--- chaveCriptografia.py
+++ chaveCriptografia.py
@@ -3,7 +3,34 @@ class ChaveCriptografia:
     def __init__(self, chave: str, criacao: datetime, validade: datetime):
         self.chave = chave
         self.criacao = criacao
-        self.validade = validade
+        self.validade = validade    
+        
+    @property
+    def chave(self):
+        return self.__chave
+    
+    @chave.setter
+    def chave(self, chave):
+        self.__chave = chave
+
+    @property
+    def criacao(self):
+        return self.__criacao
+    
+    @criacao.setter
+    def criacao(self, criacao):
+        self.__criacao = criacao
+
+    @property
+    def validade(self):
+        return self.__validade
+    
+    @criacao.setter
+    def validade(self, validade):
+        self.__validade = validade
+
+        
+    
 
     def __str__(self):
         return f'''
