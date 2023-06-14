56891bcc846dd8c0221b2bdc71501dc6afcbb0fb
Lucas Gabriel <127156538+LuCaS-apenas@users.noreply.github.com>
2023-06-14 20:09:40 -0300

    Update sistemaArquibo.py

--- sistemaArquibo.py
+++ sistemaArquibo.py
@@ -1,9 +1,12 @@
+import datetime
+from chaveCriptografia import ChaveCriptografia
+from arquivo import *
+
 class SistemaArquivos:
 
     def __init__(self, chave: ChaveCriptografia):
         self.chave_criptografia = chave
 
-        
     @property 
     def chave_criptografia(self):
         return self.__chave_criptografia
@@ -11,21 +14,18 @@ class SistemaArquivos:
     @chave_criptografia.setter
     def chave_criptografia(self, chave):
         self.__chave_criptografia = chave
-        
-        
+
     # função para gerar uma senha para acessar o arquivo.
-    def gerar_chave_criptografia(self):
-        caracteres = ''
-        while len ( caracteres ) != 11:
-            x = random.choice ( 'abcdefghijklmnopqrstuxyzABCDEFGHIJKLMNOPQRSTUVXTZ123456789#@!%¨&*' )
-            caracteres += x
-        return caracteres
+    def gerar_chave_criptografia(self, chave):
+        data_validade = datetime.datetime.now() + datetime.timedelta(days=30)
+        data_criacao = datetime.datetime.now()
+        return ChaveCriptografia(chave, data_criacao, data_validade)
 
     def revogar_chave_criptografia(self):
         pass
 
-    def listar_diretorios(self, no = None):
-        return ArvoreBinaria.imprimir( no )
+    def listar_diretorios(self,):
+        pass
 
     def gravar_arquivo_cifrado(self, arquivo: Arquivo):
         pass
