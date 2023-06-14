3467bf6b45cca50a11aa6ffa90479668ec1f5e46
ramonlbb <128474790+ramonlbb@users.noreply.github.com>
2023-06-14 20:04:33 -0300

    Update diretorio.py

--- diretorio.py
+++ diretorio.py
@@ -2,6 +2,14 @@ class Diretorio:
 
     def __init__(self, nome: str):
         self.nome = nome
+        
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
     def __str__(self):
         return f'{self.nome}'
