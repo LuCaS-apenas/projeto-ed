f4dc143d0534b4ce3b23b2116423105605c869c3
l3l0ch <118216413+l3l0ch@users.noreply.github.com>
2023-06-18 10:59:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -48,9 +48,25 @@ class ChaveCriptografia:
         self.chave = chave
         time = str(datetime.datetime.now())
         validade = str(datetime.datetime.now() + datetime.timedelta(days=30))
-        self.criacao = f'''Criado em: {time[8:10]}-{time[5:7]}-{time[0:4]} às {time[11:19]} '''
-        self.validade = f'''Chave Expira em: {validade[8:10]}-{validade[5:7]}-{validade[0:4]} às {validade[11:19]} '''
+        self.__criacao = f'''Criado em: {time[8:10]}-{time[5:7]}-{time[0:4]} às {time[11:19]} '''
+        self.__validade = f'''Chave Expira em: {validade[8:10]}-{validade[5:7]}-{validade[0:4]} às {validade[11:19]} '''
+    
+    
+    @property
+    def criacao(self):
+        return self.__criacao
 
+    @property
+    def validade(self):
+        return self.__validade
+    
+    @property
+    def chave(self):
+        return self.__chave
+    
+    @chave.setter
+    def chave(self, nova):
+        self.__chave = nova
 
     def __str__(self):
         return f'''
